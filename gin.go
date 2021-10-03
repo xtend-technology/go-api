5b100fc9a06e936ada4c75c4169c7ee5c656f41c
Tim <tim@DESKTOP-IFMFRPP.localdomain>
2021-10-04 10:00:18 +1100

    First commit

---
+++ gin.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+var db *gorm.DB
+var err error
+
+func main() {
+
+	r := gin.Default()
+
+	// USER := os.Getenv("MYSQL_USER")
+	// PASS := os.Getenv("MYSQL_PASSWORD")
+	// HOST := os.Getenv("MYSQL_HOST")
+	// DBNAME := os.Getenv("MYSQL_DB")
+
+	USER := "root"
+	PASS := "secret"
+	HOST := "mysql"
+	DBNAME := "test"
+
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS,
+		HOST, DBNAME)
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	db.AutoMigrate(&Product{})
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	} else {
+		log.Println("Connection Established")
+		// Get the first record ordered by primary key
+
+	}
+
+	r.GET("/", GetProducts)
+
+	r.Run()
+}
+
+func GetProducts(c *gin.Context) {
+	var products []Product
+	if err := db.Find(&products).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, products)
+	}
+}
